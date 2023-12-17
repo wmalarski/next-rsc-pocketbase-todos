49ed9ab88c61b4bb954f8a1102998159c94778ff
Wojciech Malarski <w.malarski@gmail.com>
2023-12-17 15:10:20 +0100

    adds error page and heading component

---
+++ src/app/(protected)/list/error.tsx
@@ -0,0 +1,39 @@
+"use client"; // Error components must be Client Components
+
+import { Button } from "@/components/Button/Button";
+import { Heading } from "@/components/Heading/Heading";
+import { flex } from "@/styled-system/patterns";
+import { useEffect } from "react";
+
+type ErrorProps = {
+  error: Error & { digest?: string };
+  reset: () => void;
+};
+
+export default function Error({ error, reset, ...props }: ErrorProps) {
+  console.log("props", props);
+
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  const onReloadClick = () => {
+    reset();
+  };
+
+  return (
+    <div
+      className={flex({
+        gap: 4,
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 4,
+      })}
+    >
+      <Heading as="h2">Something went wrong!</Heading>
+      <Button onClick={onReloadClick}>Try again</Button>
+    </div>
+  );
+}
